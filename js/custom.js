7767a2bdb3809fc88f32f7b003dd2fbf84b9ef6d
deathdance <metalmorka@gmail.com>
2015-01-26 07:11:15 +0000

    limpieza

--- js/custom.js
+++ js/custom.js
@@ -2,22 +2,22 @@ var mainslider;
 
 $(document).ready(function(){
     var options = {
-        slides: '.slide', // The name of a slide in the slidesContainer
-        swipe: true,    // Add possibility to Swipe > note that you have to include touchSwipe for this
-        transition: "slide", // Accepts "slide" and "fade" for a slide or fade transition
-        slideTracker: true, // Add a UL with list items to track the current slide
-        slideTrackerID: 'slideposition', // The name of the UL that tracks the slides
-        slideOnInterval: true, // Slide on interval
-        interval: 6000, // Interval to slide on if slideOnInterval is enabled
-        animateDuration: 2000, // Duration of an animation
-        animationEasing: 'ease', // Accepts: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
-        pauseOnHover: false // Pause when user hovers the slide container
+        slides: '.slide', // El nombre del slide en el slidesContainer
+        swipe: true,    // Añade la posibilidad de Swipe > tienes que añadir touchSwipe para esto
+        transition: "slide", // Acepta "slide" y "fade" para un slide o fade de transicion
+        slideTracker: true, // Añade un UL con una lista de objetos para rastrear el slide actual
+        slideTrackerID: 'slideposition', // El nombre del UL que rastrea los slides
+        slideOnInterval: true, // Intervalos en el Slide
+        interval: 12000, // Intervalos en el slide si slideOnInterval esta habilitado
+        animateDuration: 3000, // Duracion de la animacion
+        animationEasing: 'ease', // Acepta: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
+        pauseOnHover: false // Pausa cuando el usuario pasa por encima del contenedor del slide
     };
 
     $(".slider").simpleSlider(options);
     mainslider = $(".slider").data("simpleslider");
-    /* yes, that's all! */
 
+    // Funcion fade para antes del transito del slide
     $(".slider").on("beforeSliding", function(event){
         var prevSlide = event.prevSlide;
         var newSlide = event.newSlide;
@@ -25,12 +25,14 @@ $(document).ready(function(){
         $(".slider .slide[data-index='"+newSlide+"'] .slidecontent").hide();
     });
 
+    // Funcion fade para despues del transito del slide
     $(".slider").on("afterSliding", function(event){
         var prevSlide = event.prevSlide;
         var newSlide = event.newSlide;
         $(".slider .slide[data-index='"+newSlide+"'] .slidecontent").fadeIn();
     });
 
+    // Establecer fondos a los diferentes Slides
     $(".slide#first").backstretch("images/europa.jpg");
     $(".slide#sec").backstretch("images/saintemilion.jpg");
     $(".slide#thirth").backstretch("images/monpazier1.png");
--- js/jquery.simpleslider.js
+++ js/jquery.simpleslider.js
@@ -14,13 +14,13 @@
 */
 (function($){
     var simpleSlider = function(element, useroptions){
-        // Set some variables
+        // Establece variables
         var obj = this,
             sliderInterval = null;
         obj.currentSlide = 0;
         obj.totalSlides = 0;
 
-        // Extend default options with user options
+        // Extiende las opciones por defecto con las opciones del usuario
         useroptions = (useroptions === undefined) ? {} : useroptions;
         var options = $.extend({
             slidesContainer: element,
@@ -35,26 +35,28 @@
             animateDuration: 1000,
             animationEasing: 'ease',
             pauseOnHover: false,
-            updateTransit: true, // Change this to false is you dont want the slider to update the transit useTransitionEnd to true
+            updateTransit: true, // Cambia a falsesi no quieres que el slider actualice el transuto userTransitionEnd a verdadero
             useDefaultCSS: true
         }, useroptions);
 
+       
         /*
-         * Method to build and init the slider
+         * Metodo para crear e iniciar el slider
          */
         obj.init = function(){
-            // If transit is included and useTransitionEnd == false we will change this to true (better animation performance).
-            // Unless the user changed updateTransit to false
+            // Si el transito esta habilitado y userTransitionEnd == false , cambiaremos esto a true (mejor rendimiento de animacion). 
+            // Anoser que el usuario cambie updateTransit a false
+            
             if(options.updateTransit && $.support.transition && jQuery().transition && !$.transit.useTransitionEnd){
                 $.transit.useTransitionEnd = true;
             }
 
-            // Count the total slides
+            // Cuenta el total de slides
             obj.totalSlides = $(options.slidesContainer).find(options.slides).length;
 
             var cacheWidth = 0;
           
-            // Add default positioning css when enabled
+            // Añade posicionamiento por defecto al css cuando este habilitado
             if(options.useDefaultCSS){
                 $(options.slidesContainer).css({
                     position: "relative",
@@ -62,15 +64,15 @@
                 });
             }
 
-            // Find the slides in the sliderdom and add the index attribute
+            // Busca los slides en sliderdom y añade el atributo index
             $(options.slidesContainer).find(options.slides).each(function(index){
-                // Give each slide a data-index so we can control it later on
+                // Pone a cada slide un data-index para que podamos controlarlos mas adelante
                 $(this).attr('data-index', index);
                 cacheWidth = ($(this).outerWidth() > cacheWidth) ? $(this).outerWidth() : cacheWidth;
 
-                // Add css for slide transition
+                // Añade css para el transito del slide
                 if(options.transition == "slide"){
-                    // A fixed width is needed for the IE left animation. Here we give each slide a width
+                    // Se necesita un width fijo para la animacion izquierda del IE. Aqui se le da a cada slide un width
                     if($.support.transition !== undefined){
                         $(this).css({
                             x: index * 100 + '%',
@@ -87,9 +89,9 @@
                     }
                 }
 
-                // Add css for fade transition
+                // Añade css para la transicion de fade
                 if(options.transition == "fade"){
-                    // A fixed width is needed for the IE left animation. Here we give each slide a width
+                    // Se necesita un width fijo para la animacion izquierda del IE. Aqui se le da a cada slide un width 
                     var alpha = (index == 0) ? 1 : 0;
                     $(this).css({
                         left: 0,
@@ -100,7 +102,7 @@
                     });
                 }
 
-                // Add default positioning css when enabled
+                // Añade un posicionamiento por defecto cuando este habilitado
                 if(options.useDefaultCSS){
                     $(this).css({
                         position: "absolute",
@@ -111,89 +113,89 @@
                 }
             });
 
-            // Place the slideTracker after the container if enabled in the options
+            // Introduce el slideTraker despues del contenedor si se habilita esta opcion
             if(options.slideTracker){
-                // Add the slideposition div and add the indicators
+                // Añade el slideposition div y los indicadores
                 $(options.slidesContainer).after("<div id='"+ options.slideTrackerID +"'><ul></ul></div>");
                 for(var x = 0; x < obj.totalSlides;x++){
                     $('#'+ options.slideTrackerID +' ul').append('<li class="indicator" data-index="'+x+'"></li>');
                 }
                 $('#'+ options.slideTrackerID +' ul li[data-index="'+obj.currentSlide+'"]').addClass('active');
 
-                // Make the slide indicators clickable
+                // Hace los indicadores del slide clicables
                 $("#"+ options.slideTrackerID +" ul li").click(function(){
                     if(!($(this).hasClass("active")))
                         obj.nextSlide($(this).data('index'));
                 });
             }
 
-            // Start the slider interval if enabled in options
+            // Empiezan los intervalos del slider si se habilita
             if(options.slideOnInterval){
                 setSliderInterval();
             }
 
-            // Change the cursor with a grabbing hand that simulates the swiping on desktops
+            // Cambia el cursor a una mano agarrando para simular el movimiento de escritorios
             if(options.swipe || options.magneticSwipe){
-                // Set grabbing mouse cursor
+                // Establece la mano que agarra
                 $(options.slidesContainer).css('cursor','-webkit-grab');
                 $(options.slidesContainer).css('cursor','-moz-grab');
                 $(options.slidesContainer).css('cursor','grab');
 
-                // Set vars for swiping
+                // Establece variables para el movimiento
                 var isDragging = false;
                 var startPosition = {x: 0, y: 0};
                 var percentageMove = 0;
                 var slideWidth = $(options.slidesContainer).width();
 
-                // Check which touch events to use
+                // Comprueba que eventos tactiles se usaran
                 var touchstartEvent = (window.navigator.msPointerEnabled) ? "MSPointerDown" : "touchstart";
                 var touchmoveEvent = (window.navigator.msPointerEnabled) ? "MSPointerMove" : "touchmove";
                 var touchendEvent = (window.navigator.msPointerEnabled) ? "MSPointerUp" : "touchend";
 
-                // Bind the mousedown or touchstart event
+                // Enlaza el evento mousedown o el touchstart
                 $(options.slidesContainer).on(touchstartEvent + " mousedown", function(e){
-                    // Set isDragging on true
+                    // Establece isDragging a verdadero
                     isDragging = true;
                     
-                    // Save start coordinates
+                    // Guarda coodernadas de inicio
                     startPosition = {
                         x: (e.pageX != undefined) ? e.pageX : e.originalEvent.touches[0].pageX,
                         y: (e.pageY != undefined) ? e.pageY : e.originalEvent.touches[0].pageY
                     };
 
-                    // Reset transition animation
+                    // Resetea la animacion de transicion
                     if(options.magneticSwipe)
                         $(options.slidesContainer).find(options.slides).css('transition', 'none');
 
-                    // Reset percentage move
+                    // Resetea el porcentaje de movimiento
                     percentageMove = 0;
 
-                    // Set mouse cursor
+                    // Establece el cursor del raton
                     $(options.slidesContainer).css('cursor','-webkit-grabbing');
                     $(options.slidesContainer).css('cursor','-moz-grabbing');
                     $(options.slidesContainer).css('cursor','grabbing');
                 });
 
-                // Bind the mousemove or touchmove event
+                // Enlaza el evento mousemove o touchmove
                 $(options.slidesContainer).on(touchmoveEvent + " mousemove", function(e){
                     if(isDragging){
-                        // Calculate given distance in pixels to percentage
+                        // Calcula la distancia dada en pixels a porcentaje
                         var x = (e.pageX != undefined) ? e.pageX : e.originalEvent.touches[0].pageX;
                         percentageMove = ((startPosition.x - x) / slideWidth) * 100;
 
-                        // Check if magnetic swipe is on
+                        // Comprueba si el movimiento magnetico esta habilitado
                         if(options.magneticSwipe){
-                            // Move slides
+                            // Mueve los slides
                             obj.manualSlide(percentageMove);    
                         }
                     }
                 });
 
-                // Bind the mouseup or touchend event
+                // Enlaza el evento mouseup o touchend
                 $(options.slidesContainer).on(touchendEvent + " mouseup", function(e){
                     isDragging = false;
 
-                    // Check if we have to call the next or previous slide, or reset the slides.
+                    // Comprueba si tenemos que llamar al siguiente slide o al anterior, o resetear los slides.
                     if((percentageMove > 25 && obj.currentSlide < (obj.totalSlides - 1)))
                         obj.nextSlide();
                     else if(percentageMove < -25 && obj.currentSlide > 0)
@@ -201,26 +203,26 @@
                     else
                         obj.resetSlides();
 
-                    // Reset mouse cursor
+                    // Resetea el cursor del raton
                     $(options.slidesContainer).css('cursor','-webkit-grab');
                     $(options.slidesContainer).css('cursor','-moz-grab');
                     $(options.slidesContainer).css('cursor','grab');
                 });
             }
             
-            // Add on init event
+            // Añade evento al init
             $(element).trigger({
                 type: "init",
                 totalSlides: obj.totalSlides
             });
         }();
 
-        // Bind the function that recalculates the width of each slide on a resize.
+        // Enlaza una funcion que recalcula el width de cada slide en cada cambio de tamaño.
         $(window).resize(function(){
             var cacheWidth = 0;
         
             $(options.slidesContainer).find(options.slides).each(function(index){
-                // Reset width; otherwise it will keep the same width as before
+                // Resetea el width: sino se mantendra el mismo width que antes
                 $(this).css('width','');
                 
                 cacheWidth = ($(this).outerWidth() > cacheWidth) ? $(this).outerWidth() : cacheWidth;
@@ -230,7 +232,7 @@
         });
 
         /*
-         * Set the slider interval
+         * Establece los intervalos del slide
          */
         function setSliderInterval(){
             clearInterval(sliderInterval);
@@ -240,12 +242,12 @@
         };
 
         /*
-         * Manual offset the slider with the given percentage
+         * Compensa manualmente el slider con el porcentaje dado
          *
          * @param int percentage
          */
         obj.manualSlide = function(percentage){
-            // Move the slides based on the calculated percentage
+            // Mueve los slides basados en un porcentaje calculado
             $(options.slidesContainer).find(options.slides).each(function(index){
                 if(options.transition == "slide"){                    
                     if ($.support.transition && jQuery().transition)
@@ -257,7 +259,7 @@
         };
 
         /*
-         * Reset slides to their given position. Used after a manualSlide action
+         * Resetea los slides a su posicion dada. Usado despues de una accion manualSlide
          */
         obj.resetSlides = function(){
             $(options.slidesContainer).find(options.slides).each(function(index){
@@ -271,7 +273,7 @@
         };
 
         /*
-         * Go to the previous slide, calls the last slide when no previous slides
+         * Ir al slide previo, llama al ultimo slide cuando no hay slides previos
          */
         obj.prevSlide = function(){
             var slide = (obj.currentSlide > 0) ? obj.currentSlide -= 1 : (obj.totalSlides - 1);
@@ -279,13 +281,13 @@
         };
 
         /*
-         * Go to a next slide (function is also used for the previous slide and goto slide functions).
-         * If a paramater is given it will go to the given slide  
+         * Va al siguiente slide (la funcion tambien es usada para el slide anterior y para ir a las funciones slides)
+         * Si se le introduce un parametro este ira al correspondiente slide
          *
          * @param1 int slide
          */
         obj.nextSlide = function(slide){
-            // Cache the previous slide number and set slided to false
+            // Guarda en cache el numero de slide anterior y establece el slided a falso
             var prevSlide = obj.currentSlide,
                 slided = false;
 
@@ -294,14 +296,14 @@
             else
                 obj.currentSlide = slide;
 
-            // Create trigger point before a slide slides. Trigger wil return the prev and coming slide number
+            // Crea un trigger antes de que un slide se deslice. El trigger devuelve el numero del slide anteriro y del siguiente
             $(element).trigger({
                 type: "beforeSliding",
                 prevSlide: prevSlide,
                 newSlide: obj.currentSlide
             });
 
-            // Slide animation, here we determine if we can use CSS transitions (transit.js) or have to use jQuery animate
+            // Animacion de Slide, aqui determinamos si podemos usar transiciones de CSS (transit.js) o tener que usar animacion JQuery
             $(options.slidesContainer).find(options.slides).each(function(index){
             
                 if(options.transition == "slide"){                    
@@ -326,10 +328,10 @@
             
             });
 
-            // Somehow the callback from $.transition doesn't work, so we create ow custom bind here
+            // Si por algun motivo el callback de $.transition no funciona, creamos nuestro propio enlace aqui
             $(options.slidesContainer).on('oTransitionEnd webkitTransitionEnd oTransitionEnd otransitionend transitionend', triggerSlideEnd);
 
-            // Create trigger point after a slide slides. All the slides return a TransitionEnd; to prevent a repeating trigger we keep a slided var
+            // Creamos un trigger despues del movimiento del slide. Todos los slides devuelven un TransitionEnd; para prevenir la repeticion de un trigger, mantenemos la variable slided
             function triggerSlideEnd(){
                 if(!slided){
                     if(options.transition == "fade"){
@@ -343,7 +345,7 @@
                         });
                     }
                 
-                    // Trigger event
+                    // Evento Trigger
                     $(element).trigger({
                         type: "afterSliding",
                         prevSlide: prevSlide,
@@ -354,18 +356,18 @@
                 }
             }
 
-            // Show current slide bulb
+            // Muestra el monton de slide actual
             $('#'+ options.slideTrackerID +' ul li').removeClass('active');
             $('#'+ options.slideTrackerID +' ul li[data-index="'+obj.currentSlide+'"]').addClass('active');
 
-            // (Re)set the slider interval
+            // Resetea/Establece el intervalo del slide
             if(options.slideOnInterval){
                 setSliderInterval();
             }
         };
 
-        // Function for the pauseOnHover.
-        //The function will clear the interval and restart it after the mouse disappears from the container
+        // Funcion para el pauseOnHover.
+        // La funcion dejara en blanco el intervalo y lo reseteara despues de que el raton desaparezca del contenedor
         if(options.pauseOnHover){
             $(options.slidesContainer).hover(function(){
                 clearInterval(sliderInterval);
@@ -375,18 +377,18 @@
         }
     };
 
-    // Create a plugin
+    // Crea un pluggin
     $.fn.simpleSlider = function(options){
         return this.each(function(){
             var element = $(this);
 
-            // Return early if this element already has a plugin instance
+            // Devuelve antes de tiempo si este elemento ya tiene un pluggin instanciado
             if (element.data('simpleslider')) return;
 
-            // Pass options and element to the plugin constructer
+            // Pasa opciones y elementos al contenedor del pluggin
             var simpleslider = new simpleSlider(this, options);
 
-            // Store the plugin object in this element's data
+            // Almacena el objeto del pluggin en lso datos de este elemento
             element.data('simpleslider', simpleslider);
         });
     }
